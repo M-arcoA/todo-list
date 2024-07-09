3f760c1df77e9ffc74b8034b17a811c68af49309
dyovanamenegattibefore <dyovana.menegatti@before.com.br>
2024-07-08 21:03:39 -0300

    Adiciona biblioteca toasted

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'font-awesome/css/font-awesome.css';
+import Toasted from 'vue-toasted'
 
 Vue.config.productionTip = false
 
